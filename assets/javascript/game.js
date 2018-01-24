72f8c21ed127e99a92fdb4c81f6d8a84bc151b45
Kate Birmingham <kabi6686@colorado.edu>
2018-01-23 23:32:34 -0700

    added onclick for each crystal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,22 @@
 $(function () {
 
-	$()
+	$(".redCrystal").on("click", function() {
+		alert("you clicked the red crystal");
+	}); //END RED CRYSTAL CLICK
+
+	$(".greenCrystal").on("click", function() {
+		alert("you clicked the green crystal");
+	}); //END GREEN CRYSTAL CLICK
+
+	$(".darkblueCrystal").on("click", function() {
+		alert("you clicked the darkblue crystal");
+	}); //END DARKBLUE CRYSTAL CLICK
+
+	$(".lightblueCrystal").on("click", function() {
+		alert("you clicked the lightblue crystal");
+	}); //END LIGHTBLUE CRYSTAL CLICK
+
+
 
 
 }); // END READY
